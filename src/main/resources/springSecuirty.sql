0307776f442acd771d995fd5082a601fccdd3d39
PrashhanthNelakanti <prashhanth.nelakanti@gmail.com>
2021-05-20 20:20:16 +0530

    added sql file

---
+++ src/main/resources/springSecuirty.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM spring_security.user
+LIMIT 0, 1000
+
+-- Date: 2021-05-20 20:17
+*/
+INSERT INTO `` (`user_id`,`is_active`,`name`,`role`,`password`) VALUES (1,'1','Douglas','ROLE_USER','1234');
+INSERT INTO `` (`user_id`,`is_active`,`name`,`role`,`password`) VALUES (2,'1','Diwas','ROLE_ADMIN','asdf');
+INSERT INTO `` (`user_id`,`is_active`,`name`,`role`,`password`) VALUES (3,'1','Dev','ROLE_USER','albert');
+																							--    password = albert 
+INSERT INTO `` (`user_id`,`is_active`,`name`,`role`,`password`) VALUES (4,'1','Joy','ROLE_ADMIN','$2a$10$s3QknQjaWw.Dwmt7hOiFhuh25J9h6qPK5nZF2rI/pGnQqDJR2wYC2');
