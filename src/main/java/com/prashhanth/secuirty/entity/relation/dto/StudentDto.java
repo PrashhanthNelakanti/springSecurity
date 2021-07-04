2424b8c058404ace74bafbdbbce0c813bc8ab1e7
prashhanthsoujanya <prashhanth.nelakanti@gmail.com>
2021-07-05 01:35:50 +0530

    OneToMany

---
+++ src/main/java/com/prashhanth/secuirty/entity/relation/dto/StudentDto.java
@@ -0,0 +1,13 @@
+package com.prashhanth.secuirty.entity.relation.dto;
+
+import com.prashhanth.secuirty.entity.relation.Student;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentDto {
+    private Student student;
+}
