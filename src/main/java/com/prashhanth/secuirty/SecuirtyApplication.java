d708780d6fc80fbf0622293c6b4e42b3b4fa40ae
prashhanthsoujanya <prashhanth.nelakanti@gmail.com>
2021-08-15 01:01:54 +0530

    converted to war file

--- src/main/java/com/prashhanth/secuirty/SecuirtyApplication.java
+++ src/main/java/com/prashhanth/secuirty/SecuirtyApplication.java
@@ -3,10 +3,17 @@ package com.prashhanth.secuirty;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableEncryptableProperties
-public class SecuirtyApplication {
+public class SecuirtyApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SecuirtyApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecuirtyApplication.class, args);
