4b5ec72c3f3493fc94254372a2dee334494dadd5
prashhanthsoujanya <prashhanth.nelakanti@gmail.com>
2022-01-03 12:27:01 +0530

    default add

--- src/main/resources/ang/src/app/components/main-page/reachme/contact.ts
+++
@@ -1,7 +0,0 @@
-export class Contact {
-    contactId: number;
-    contactName: string;
-    org: string;
-    descrption: string;
-    status:string;
-}
--- src/main/resources/ang/src/app/user.ts
+++
@@ -1,7 +0,0 @@
-export class User {
-    id:number;
-    name:String;
-    location:String;
-    salary:number;
-   }
-   
